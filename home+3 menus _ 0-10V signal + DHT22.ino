92437de8094093ec9ab6ddefbf381ab5f94731c2
Palaskel <jesaistropas@gmail.com>
2017-08-24 19:22:10 +0200

    Update home+3 menus _ 0-10V signal + DHT22.ino

--- home+3 menus _ 0-10V signal + DHT22.ino
+++ home+3 menus _ 0-10V signal + DHT22.ino
@@ -69,3 +69,183 @@ void setTemp() {
   }
   
   if (digitalRead(plus_key) == LOW)
+  {
+    if(setTempVal < 30)
+    {
+      ++setTempVal;
+    }
+    lcd.clear();
+  }
+}  
+
+void setMaxSpeed() {
+  if (digitalRead(less_key) == LOW)
+  {
+    if (MaxSpeedPerc > 0)
+    {
+      MaxSpeedPerc -= IncPerc;    
+    }
+    lcd.clear();
+  }
+  
+  if (digitalRead(plus_key) == LOW)
+  {
+    if(MaxSpeedPerc < 100)
+    {
+      MaxSpeedPerc += IncPerc;
+    }
+    lcd.clear();
+  }
+}
+
+void setMinSpeed() {
+  if (digitalRead(less_key) == LOW)
+  {
+    if (MinSpeedPerc > 0)
+    {
+      MinSpeedPerc -= IncPerc;    
+    }
+    lcd.clear();
+  }
+  
+  if (digitalRead(plus_key) == LOW)
+  {
+    if(MinSpeedPerc < 100)
+    {
+      MinSpeedPerc += IncPerc;
+    }
+    lcd.clear();
+  }
+}
+
+void printHome() {
+  lcd.setCursor(0, 0);
+  lcd.print("Flo: ");
+  lcd.print(FloTemp);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Veg: ");
+  lcd.print(VegTemp);
+  lcd.print("C");
+}
+
+void printTemp() {
+  lcd.setCursor(0, 0);
+  lcd.print("Set Temp.");
+  lcd.setCursor(0,  1);
+  lcd.print(setTempVal);
+  lcd.print("C");
+}
+
+void printMaxSpeed() {
+  lcd.home();
+  lcd.print("Set Max Speed");
+  lcd.setCursor(0, 1);
+  lcd.print(MaxSpeedPerc);
+  lcd.print("%");
+}
+
+void printMinSpeed() {
+  lcd.home();
+  lcd.print("Set Min Speed");
+  lcd.setCursor(0, 1);
+  lcd.print(MinSpeedPerc);
+  lcd.print("%");
+}
+
+//=================================================================
+//                  SETUP
+//=================================================================
+void setup() {
+  pinMode(SpeedPin, OUTPUT);
+  pinMode(up_key,INPUT);
+  pinMode(down_key,INPUT);
+  pinMode(less_key,INPUT);
+  pinMode(plus_key,INPUT);
+  
+  digitalWrite(up_key,HIGH); //Pull up 
+  digitalWrite(down_key,HIGH);
+  digitalWrite(less_key,HIGH);
+  digitalWrite(plus_key,HIGH);//Pull up
+  
+  lcd.begin(16, 2);
+  lcd.print(" ClimateControl");
+  delay(1500);
+  lcd.clear();
+}
+
+//=================================================================
+//                  LOOP
+//================================================================= 
+void loop() {
+  
+  if(digitalRead(down_key)==LOW)
+  {
+    if(menuCount < menuCountMax)
+    {
+      menuCount++;    
+    }
+    else {
+      menuCount = menuCountMax;
+    }
+    lcd.clear();
+  }
+  
+  if(digitalRead(up_key)==LOW)
+  {
+    if(menuCount > 1)
+    {
+      menuCount--;
+    }
+    else {
+      menuCount = 1;
+    }
+    lcd.clear();
+  }
+  
+  if(menuCount == 1) {
+    getData();
+    printHome();
+  }
+  
+  if(menuCount == 2) {
+    setTemp();
+    printTemp();
+  }
+  
+  if(menuCount == 3) {
+    setMaxSpeed();
+    printMaxSpeed();
+  }
+  
+  if(menuCount == 4) {
+    setMinSpeed();
+    printMinSpeed();
+  }
+  
+  HysteresisPerc = (MaxSpeedPerc-MinSpeedPerc)/5;
+  if (FloTemp <= setTempVal) {
+    SpeedVal = map(MinSpeedPerc, 0, 100, 0, 255);
+    analogWrite(SpeedPin, SpeedVal);
+  }
+  else if (FloTemp > setTempVal && FloTemp <= setTempVal+1) {
+    SpeedVal = map(HysteresisPerc+MinSpeedPerc, 0, 100, 0, 255);
+    analogWrite(SpeedPin, SpeedVal);
+  }
+  else if (FloTemp > setTempVal+1 && FloTemp <= setTempVal+2) {
+    SpeedVal = map((HysteresisPerc*2)+MinSpeedPerc, 0, 100, 0, 255);
+    analogWrite(SpeedPin, SpeedVal);
+  }
+  else if (FloTemp > setTempVal+2 && FloTemp <= setTempVal+3) {
+    SpeedVal = map((HysteresisPerc*3)+MinSpeedPerc, 0, 100, 0, 255);
+    analogWrite(SpeedPin, SpeedVal);
+  }
+  else if (FloTemp > setTempVal+3 && FloTemp <= setTempVal+4) {
+    SpeedVal = map((HysteresisPerc*4)+MinSpeedPerc, 0, 100, 0, 255);
+    analogWrite(SpeedPin, SpeedVal);
+  }
+  else if (FloTemp > setTempVal+4) {
+    SpeedVal = map(MaxSpeedPerc, 0, 100, 0, 255);
+    analogWrite(SpeedPin, SpeedVal);
+  }
+}
